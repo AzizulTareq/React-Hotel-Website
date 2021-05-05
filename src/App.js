0daa765dc1cf17222a02c509c56c15fcf9f9f5ba
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-05-06 03:29:51 +0600

    background image set up for every pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+import { Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      hello from app
-    </div>
+    <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Route component={Error} />
+    </Switch>   
+    </>
   )
 }
 
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+    return (
+        <div>
+            Hi from single room
+        </div>
+    )
+}
+
+export default SingleRoom
